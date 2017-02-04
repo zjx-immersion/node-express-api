656dee7f48e67bc91c6e1d78ee84f1740b73e6ed
zjx-immersion <jianxin_home@163.com>
2017-02-04 22:57:24 +0800

    [Jason] set up the gulp and babel env for dev in ES7 of coding

---
+++ gulpfile.babel.js
@@ -0,0 +1,48 @@
+import gulp from 'gulp';
+import shell from 'gulp-shell';
+import rimraf from 'rimraf';
+import run from 'run-sequence';
+import watch from 'gulp-watch';
+import server from 'gulp-live-server';
+
+const paths = {
+    js: ['./src/**/*.js'],
+    destination: './app'
+}
+
+gulp.task('default', cb => {
+    run('server', 'build', 'watch', cb);
+});
+
+gulp.task('build', cb => {
+    run('clean', 'flow', 'babel', 'restart', cb);
+});
+
+gulp.task('clean', cb => {
+    rimraf(paths.destination, cb);
+});
+
+gulp.task('flow', shell.task(['flow'], { ignoreErrors: true }));
+
+// gulp.task('flow', () => {
+//     return gulp.src('./node_modules')
+//         .pipe(flow({ killFlow: false }));
+// });
+
+gulp.task('babel', shell.task(['babel src --out-dir app']));
+
+let express;
+
+gulp.task('server', () => {
+    express = server.new(paths.destination);
+});
+
+gulp.task('restart', () => {
+    express.start.bind(express)();
+});
+
+gulp.task('watch', () => {
+    return watch(paths.js, () => {
+        gulp.start('build');
+    });
+});
